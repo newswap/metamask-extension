bac8e0dd67ad1482e9e1fb31d16add52e05c6598
Meng Guang <mengguang@newtonproject.org>
2020-10-09 10:51:54 +0800

    fix terms-of-use

--- ui/app/pages/first-time-flow/create-password/new-account/new-account.component.js
+++ ui/app/pages/first-time-flow/create-password/new-account/new-account.component.js
@@ -208,7 +208,7 @@ export default class NewAccount extends PureComponent {
                 <a
                   onClick={(e) => e.stopPropagation()}
                   key="first-time-flow__link-text"
-                  href="https://metamask.io/terms.html"
+                  href="https://www.newtonproject.org/terms-of-use/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
