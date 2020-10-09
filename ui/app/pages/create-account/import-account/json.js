b2af684b8c08f9325c527aad1b52adad9e88ce61
Meng Guang <mengguang@newtonproject.org>
2020-10-09 10:37:08 +0800

    hide help link

--- ui/app/pages/create-account/import-account/json.js
+++ ui/app/pages/create-account/import-account/json.js
@@ -9,7 +9,7 @@ import { getMetaMaskAccounts } from '../../../selectors'
 import Button from '../../../components/ui/button'
 import { getMostRecentOverviewPage } from '../../../ducks/history/history'
 
-const HELP_LINK = 'https://metamask.zendesk.com/hc/en-us/articles/360015489331-Importing-an-Account'
+// const HELP_LINK = 'https://metamask.zendesk.com/hc/en-us/articles/360015489331-Importing-an-Account'
 
 class JsonImportSubview extends Component {
   state = {
@@ -25,8 +25,8 @@ class JsonImportSubview extends Component {
 
     return (
       <div className="new-account-import-form__json">
-        <p>{this.context.t('usedByClients')}</p>
-        <a className="warning" href={HELP_LINK} target="_blank" rel="noopener noreferrer">{this.context.t('fileImportFail')}</a>
+        {/* <p>{this.context.t('usedByC lients')}</p>*/}
+        {/* <a className="warning" href={HELP_LINK} target="_blank" rel="noopener noreferrer">{this.context.t('fileImportFail')}</a>*/}
         <FileInput
           readAs="text"
           onLoad={this.onLoad.bind(this)}
